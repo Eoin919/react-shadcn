d7daa035a50ccef4acc96b86778fd11a14cd318c
Eoin MacCarthy <eoinmaccarthy0919@gmail.com>
2025-07-29 11:20:01 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import AppProvider from './providers';
 import AppRouter from './routes';
 
-export default function App() {
-  return (
-    <AppProvider>
-      <AppRouter />
-    </AppProvider>
-  );
-}
+const App = () => (
+  <AppProvider>
+    <AppRouter />
+  </AppProvider>
+);
+
+export default App;
